a4c0aea53aff8ab1c3fd41e4f043c3d5d3b88ed4
Marissa Ortega <marissaljames@gmail.com>
2022-11-02 03:03:01 +0000

    styled redirect page to match the rest of the app

--- client/pages/not-found.jsx
+++ client/pages/not-found.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default function NotFound(props) {
   return (
     <div>
-      <h3>
+      <h3 className="mt-5 text-center">
         Uh oh, we could not find the page you were looking for!
       </h3>
-      <p>
-        <a href="#">Return to Home Page</a>
+      <p className="mt-3 text-center">
+        <a href="#" className="return-home">Click here to return to Home Page</a>
       </p>
     </div>
   );
